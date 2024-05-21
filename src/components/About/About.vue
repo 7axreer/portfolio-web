b5f017a0737c77a6abc07bbe2b09190522e6f364
axreer <saidaxrorpc7@gmail.com>
2024-05-21 20:49:38 +0500

    changed

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -8,7 +8,6 @@
                     <p class="main__des">
                         Let me introduce myself, my workflows, my collaborations, and the technologies I like to use to bring my projects to life.
                     </p>
-                    <div class="line"></div>
                 </div>
 
                 <div class="aboutMe">
@@ -25,17 +24,92 @@
                             <div class="info__main">
                                 <img src="@/assets/img/me.png" alt="" class="aboutMe__img" />
                                 <ol class="info__texts" type="1">
-                                    <li class="info__text">Hi, I'm <span class="info__firstSpan">Saidaxror</span>, a <span class="info__secondSpan">Front-End Web Developer</span>!</li>
-                                    <li class="info__text">I'm a <span class="info__thirdSpan">19 years old</span> boy from <span class="info__fifthSpan">Tashekent</span>, Uzbekistan.</li>
                                     <li class="info__text">
-                                        I focus is on <span class="info__thirdSpan">creative development</span>, where I leverage my skills to bring <span class="info__fourthSpan">your vision</span> to <span class="info__fifthSpan">life</span> by combining <span class="info__secondSpan">creativity</span> with <span class="info__secondSpan">technical proficiency</span>.
+                                        Hi, I'm <span class="info__firstSpan">Saidaxror</span>, a
+                                        <span class="info__secondSpan">Front-End Web Developer</span>!
                                     </li>
-                                    <li class="info__text"><span class="info__fifthSpan">Now</span>, I'm working as a freelancer in <span class="info__firstSpan">UPWORK</span> and handed over lots of <span class="info__fourthSpan">successful projects</span> to satisfied <span class="info__firstSpan">clients</span>.</li>
+                                    <li class="info__text">
+                                        I'm a <span class="info__thirdSpan">19 years old</span> boy from
+                                        <span class="info__fifthSpan">Tashekent</span>, Uzbekistan.
+                                    </li>
+                                    <li class="info__text">
+                                        I focus is on <span class="info__thirdSpan">creative development</span>, where I leverage my skills to bring
+                                        <span class="info__fourthSpan">your vision</span> to <span class="info__fifthSpan">life</span> by combining
+                                        <span class="info__secondSpan">creativity</span> with
+                                        <span class="info__secondSpan">technical proficiency</span>.
+                                    </li>
+                                    <li class="info__text">
+                                        <span class="info__fifthSpan">Now</span>, I'm working as a freelancer in
+                                        <span class="info__firstSpan">UPWORK</span> and handed over lots of
+                                        <span class="info__fourthSpan">successful projects</span> to satisfied
+                                        <span class="info__firstSpan">clients</span>.
+                                    </li>
                                 </ol>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="workAndStudy">
+                    <div class="workStudy">
+                        <div class="work">
+                            <div class="work__top">
+                                <p class="info__title">work experience</p>
+                                <div class="window__top">
+                                    <i class="far fa-minus"></i>
+                                    <i class="far fa-expand-wide"></i>
+                                    <i class="far fa-times"></i>
+                                </div>
+                            </div>
+                            <div class="work__main">
+                                <div class="work__box">
+                                    <p class="work__main-title">Upwork</p>
+                                    <p class="work__main-name">Front-End Developer</p>
+                                    <p class="work__main-des">
+                                        Today I work as a freelancer on the UPWORK platform. I have worked and am working on many projects untill now.
+                                    </p>
+                                </div>
+                                <div class="work__box">
+                                    <p class="work__main-title work__main-titleSecond">ASR Innovation Center</p>
+                                    <p class="work__main-name">Graphic Designer</p>
+                                    <p class="work__main-des">
+                                        I worked as a graphic designer at Asr Innovation Learning Center. I have made many SMD posts.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="workStudy studyWork">
+                        <div class="work">
+                            <div class="work__top">
+                                <p class="info__title">education</p>
+                                <div class="window__top">
+                                    <i class="far fa-minus"></i>
+                                    <i class="far fa-expand-wide"></i>
+                                    <i class="far fa-times"></i>
+                                </div>
+                            </div>
+                            <div class="work__main">
+                                <div class="work__box">
+                                    <p class="work__main-title">Upwork</p>
+                                    <p class="work__main-name">Front-End Developer</p>
+                                    <p class="work__main-des">
+                                        Today I work as a freelancer on the UPWORK platform. I have worked and am working on many projects untill now.
+                                    </p>
+                                </div>
+                                <div class="work__box">
+                                    <p class="work__main-title work__main-titleSecond">ASR Innovation Center</p>
+                                    <p class="work__main-name">Graphic Designer</p>
+                                    <p class="work__main-des">
+                                        I worked as a graphic designer at Asr Innovation Learning Center. I have made many SMD posts.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </main>
